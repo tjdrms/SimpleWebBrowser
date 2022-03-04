d3ea3e4dac5b74423eacb9c3ace85cdf8153fa3b
tjdrms <tjdrms0116>
2022-03-04 11:33:02 +0900

    심플 웹 브라우저

--- app/src/main/java/com/example/simplewebbrowser/MainActivity.kt
+++ app/src/main/java/com/example/simplewebbrowser/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.simplewebbrowser
 
 import android.annotation.SuppressLint
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.inputmethod.EditorInfo
+import android.webkit.URLUtil
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.EditText
 import android.widget.ImageButton
+import androidx.core.widget.ContentLoadingProgressBar
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,6 +36,14 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.webView)
     }
 
+    private val refreshLayout: SwipeRefreshLayout by lazy {
+        findViewById(R.id.refreshLayout)
+    }
+
+    private val progressBar: ContentLoadingProgressBar by lazy {
+        findViewById(R.id.progressBar)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -51,6 +64,7 @@ class MainActivity : AppCompatActivity() {
     private fun initViews() {
         webView.apply{
             webViewClient = WebViewClient()
+            webChromeClient = WebChromeClient()
             settings.javaScriptEnabled = true
             loadUrl(DEFAULT_URL)
         }
@@ -63,7 +77,14 @@ class MainActivity : AppCompatActivity() {
 
         addressBar.setOnEditorActionListener { textView, i, keyEvent ->
             if (i == EditorInfo.IME_ACTION_DONE) {
-                webView.loadUrl(textView.text.toString())
+                val loadingUrl = textView.text.toString()
+
+                if (URLUtil.isNetworkUrl(loadingUrl)) {
+                    webView.loadUrl(loadingUrl)
+                } else {
+                    webView.loadUrl("http://$loadingUrl")
+                }
+
             }
 
             return@setOnEditorActionListener false
@@ -75,6 +96,39 @@ class MainActivity : AppCompatActivity() {
 
         goForwardButton.setOnClickListener {
             webView.goForward()
+        }
+
+        refreshLayout.setOnRefreshListener {
+            webView.reload()
+        }
+    }
+
+
+
+    inner class WebViewClient: android.webkit.WebViewClient() {
+
+        override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+            super.onPageStarted(view, url, favicon)
+
+            progressBar.show()
+        }
+
+        override fun onPageFinished(view: WebView?, url: String?) {
+            super.onPageFinished(view, url)
+
+            refreshLayout.isRefreshing = false
+            progressBar.hide()
+            goBackButton.isEnabled = webView.canGoBack()
+            goForwardButton.isEnabled = webView.canGoForward()
+            addressBar.setText(url)
+        }
+    }
+
+    inner class WebChromeClient: android.webkit.WebChromeClient() {
+        override fun onProgressChanged(view: WebView?, newProgress: Int) {
+            super.onProgressChanged(view, newProgress)
+
+            progressBar.progress = newProgress
         }
     }
 
